9209f6316794e8c4f33255467034344ae60f7f5c
Rhythm2359 <rhythmbhandari44@gmail.com>
2025-12-08 14:43:53 +0530

    backend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,37 @@
+import React from 'react';
 import Menubar from "./components/Menubar.jsx";
 import Home from "./pages/Home.jsx";
 import Footer from "./components/Footer.jsx";
-import { Route, Routes } from "react-router-dom";
-import toast, { Toaster } from "react-hot-toast";
+import {Route, Routes} from "react-router-dom";
+import UserSyncHandler from "./components/UserSyncHandler.jsx";
+import {Toaster} from "react-hot-toast";
+import {RedirectToSignIn, SignedIn, SignedOut} from "@clerk/clerk-react";
+import Result from "./pages/Result.jsx";
+
+const ResultRoute = () => (
+    <>
+        <SignedIn>
+            <Result />
+        </SignedIn>
+        <SignedOut>
+            <RedirectToSignIn />
+        </SignedOut>
+    </>
+);
 
 const App = () => {
-  return (
-    <div>
-      <Menubar />
-      <Toaster/>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-      <Footer />
-    </div>
-  )
+    return(
+        <div>
+            <UserSyncHandler/>
+            <Menubar/>
+            <Toaster/>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/result" element={<ResultRoute />} />
+            </Routes>
+            <Footer/>
+        </div>
+    )
 }
 
 export default App;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,20 +1,24 @@
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
-import { ClerkProvider } from '@clerk/clerk-react'
+
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+
+import { ClerkProvider } from "@clerk/clerk-react";
+import { BrowserRouter } from "react-router-dom";
+import AppContextProvider from "./context/AppContext.jsx";
+
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
-if (!PUBLISHABLE_KEY) {
-  throw new Error('Missing Publishable key');
-}
+ReactDOM.createRoot(document.getElementById("root")).render(
 
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-      <App />
-    </ClerkProvider>
-  </BrowserRouter>
-)
+            <BrowserRouter>
+                <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+                    <AppContextProvider>
+                        <App/>
+                    </AppContextProvider>
+                </ClerkProvider>
+            </BrowserRouter>
+
+);
